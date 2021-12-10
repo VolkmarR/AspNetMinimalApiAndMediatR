294814b37128cb0cd184567ad412bebe7a961307
Volkmar Rigo <volkmar@rigo.bz>
2021-12-10 09:28:47 +0100

    Initial Setup

---
+++ MinimalApi/MediatRHandler/Repository/TodoRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediatRHandler.Repository
+{
+
+    public class TodoRepository
+    {
+        public record Todo(int Id, string Description, bool Done);
+
+        int _NextID = 1;
+        List<Todo> _Todo = new() { new Todo(0, "dummy", false)};
+
+        public IReadOnlyList<Todo> Get()
+            => _Todo;
+
+        public void Add(string description)
+            => _Todo.Add(new Todo(_NextID++, description, false));
+    }
+}
---
+++ MinimalApi/MediatRHandler/TodoQuery.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using MediatRHandler.Repository;
+
+namespace MediatRHandler
+{
+
+    public class TodoQuery : IRequest<List<TodoQueryDto>>
+    {
+
+    }
+
+    public record TodoQueryDto(int Id, string Description, bool Done);
+
+    public class TodoQueryHandler : IRequestHandler<TodoQuery, List<TodoQueryDto>>
+    {
+        private readonly TodoRepository _Repository;
+
+        public TodoQueryHandler(TodoRepository repository)
+        {
+            _Repository = repository;
+        }
+
+        public Task<List<TodoQueryDto>> Handle(TodoQuery request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(_Repository.Get().Select(q => new TodoQueryDto(q.Id, q.Description, q.Done)).ToList()); 
+        }
+    }
+}
